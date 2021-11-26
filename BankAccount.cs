78e0ce2007f21acbcea3774d939e4bba25ec59c0
pauloALuis <50427097+pauloALuis@users.noreply.github.com>
2021-11-26 20:57:09 +0000

    Add project files.

---
+++ BankAccount.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Objets_Classes
+{
+    class MakeDeposits
+    {
+        public DateTime DateDeposit { get; set; }
+
+        public decimal Amount { get; set; }
+        public string Note { get; set; }
+
+
+
+    }
+    /// <summary>
+    /// 
+    /// </summary>
+    class BankAccount
+    {
+        public string Number { get; }
+        public string Owner { get; set; }
+        public decimal Balance
+        {
+            get
+            {
+                decimal balance = 0;
+                foreach (var item in allTransactions)
+                {
+                    balance += item.Amount;
+                }
+                return balance;
+            }
+
+        }
+
+        private static int accountNumberSeed = 1234567890; //{ get; set; }
+        string otherName; // field
+
+        private List<Transaction> allTransactions = new List<Transaction>();
+
+
+        public string GetAccountHistory()
+        {
+            var report = new StringBuilder();
+
+            decimal balance = 0;
+            //create header
+            report.AppendLine("Date\t\tAmount\tBalance\tNote");
+            foreach (var item in allTransactions)
+            {
+                //create rows
+                balance += item.Amount;
+                report.AppendLine($"{item.Date.ToShortDateString()}- \t{item.Amount} - \t{balance}\t{item.Notes}");
+            }
+
+            return report.ToString();
+        }
+
+        public string OtherName   // property
+        {
+            get { return otherName; }   // get method
+            set { otherName = value; }  // set method
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="initialBalace"></param>
+        public BankAccount(string name, decimal initialBalance = 500)
+        {
+            this.Owner = name;//
+            //this.Balance = initialBalace;
+            this.MakeDeposit(initialBalance,DateTime.Now, "Inicial Deposit");
+            this.Number = accountNumberSeed.ToString();
+            accountNumberSeed++;
+
+        }
+
+
+        public override string ToString()
+        {
+            return $"{this.Number}, {this.Owner}, {this.Balance} ";
+        }
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="amount"></param>
+        /// <param name="date"></param>
+        /// <param name="note"></param>
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+            //  Balance += amount;
+
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
+            }
+
+
+            Transaction deposit = new Transaction(amount, date, note);
+            this.allTransactions.Add(deposit);
+        }
+
+
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="amount"></param>
+        /// <param name="date"></param>
+        /// <param name="note"></param>
+        public void MakeWithDrawal(decimal amount, DateTime date, string note)
+        {
+
+
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawal must be positive");
+            }
+            if (Balance - amount < 0)
+            {
+                throw new InvalidOperationException("Not sufficient funds for this withdrawal");
+            }
+
+            //Balance -= amount;
+            Transaction withDrawal = new Transaction(-amount, date, note);
+            this.allTransactions.Add(withDrawal);
+
+
+        }
+
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using Objets_Classes;
+using System;
+
+namespace MySuperBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var account = new BankAccount("Carlos", 1500);
+            var account1 = new BankAccount("1Carlos", 1500);
+            var account2 = new BankAccount("2Carlos");
+            Console.WriteLine($"Account {account2.ToString()}");
+
+            Console.WriteLine($"My account name= {account.Owner}, with  {account.Number}");
+            Console.WriteLine($"My account name= {account1.Owner}, with  {account1.Number}");
+            Console.WriteLine($"Account {account2.Owner}, with  {account2.Number}");
+            string note = "With Drawal  necessar";
+            Console.WriteLine($"Balance {account2.Balance}");
+            account.MakeWithDrawal(750, DateTime.Now, "Attempt to overdraw");
+            account.MakeDeposit(890, DateTime.Now, "money carlos");
+
+            string dateInput = "Jan 1, 2009";
+            var parsedDate = DateTime.Parse(dateInput);
+            account.MakeDeposit(10000, parsedDate, "money carlos");
+
+            Console.WriteLine(account.GetAccountHistory());
+
+            try
+            {
+                var invalidAccount = new BankAccount("Marta Domingos ", -55);
+               // account2.MakeWithDrawal(800, DateTime.Now, note );
+
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine($"Exception caugth creating account {account.Owner}, with  negative balance");
+                Console.WriteLine($" {e}");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine($"Erro disconhecido!!");
+               // Console.WriteLine($" {e.ToString()}");
+            }
+
+
+            // Test for a negative balance.
+            try
+            {
+                account.MakeWithDrawal(750, DateTime.Now, "Attempt to overdraw");
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine("Exception caught trying to overdraw");
+                Console.WriteLine(e.ToString());
+            }
+
+
+        }
+    }
+}
